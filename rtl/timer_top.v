91f0602c666de19dbba46a319732841184b0bf06
phuchungpt <phuchungdcdt@gmail.com>
2024-11-23 01:09:58 +0700

    Update timer_top.v

--- rtl/timer_top.v
+++ rtl/timer_top.v
@@ -20,29 +20,39 @@ module timer_top
   wire rd_en_tmp; 
   
   // register to interrupt 
-  wire 
-  wire 
-  wire 
-  wire 
+  wire int_st_tmp     ;
+  wire int_en_tmp     ; 
+  wire tisr_wr_set_tmp;
+	wire [63:0] tcmp_tmp;
 
   // register to counter control 
-  
+  wire halt_ack_tmp     ; 
+	wire div_en_tmp       ;
+	wire [3:0] div_val_tmp; 
+	wire timer_en_tmp     ; 
+	wire halt_req_tmp     ;
+	
   // register to counter 
-  
+	wire [63:0] cnt_tmp   ;
+	wire tdr0_wr_sel_tmp  ;
+	wire tdr1_wr_sel_tmp  ;
+	wire cnt_clr_tmp      ; 
+	
   // counter_control to counter 
-  
+  wire cnt_en_tmp       ;
+	
 // apb_slave 
   apb_slave u_apb_slave(
    // apb block to timer_ip 
 		.clk(sys_clk)          , 
-	   .rst_n(sys_rst_n)      , 
-	   .psel(tim_psel)        , 	
-      .pwrite(tim_pwrite)    ,
+	  .rst_n(sys_rst_n)      , 
+	  .psel(tim_psel)        , 	
+    .pwrite(tim_pwrite)    ,
 		.penable(tim_penable)  ,
 		.tim_pready(tim_pready),
 	// apb_block to register 
-	   .wr_en(wr_en_tmp)      , 
-      .rd_en(rd_en_tmp)
+	  .wr_en(wr_en_tmp)      , 
+    .rd_en(rd_en_tmp)
   ); 
   
 // register block
@@ -65,13 +75,13 @@ module timer_top
 		.tisr_wr_sel(tisr_wr_sel_tmp),
 		.tcmp(tcmp_tmp)              ,
   // register to counter_control
-	   .halt_ack(halt_ack_tmp)      , 
+		.halt_ack(halt_ack_tmp)      , 
 		.div_en(div_en_tmp)          ,
 		.div_val(div_val_tmp)        ,
 		.timer_en(timer_en_tmp)      ,
 		.halt_req(halt_req_tmp)      ,
   // register to counter 
-      .tdr0_wr_sel(tdr0_wr_sel_tmp), 
+    .tdr0_wr_sel(tdr0_wr_sel_tmp), 
 		.tdr1_wr_sel(tdr1_wr_sel_tmp), 
 		.cnt(cnt_tmp)                ,
 		.cnt_clr(cnt_clr_tmp)
@@ -80,15 +90,15 @@ module timer_top
 // counter control block 
   counter_control u_counter_control(
   // Counter_control to timer_ip 
-		.clk(sys_clk)          ,
-		.rst_n(sys_rst_n)      ,
-		.dbg_mode(dbg_mode)    ,
+      .clk(sys_clk)          ,
+      .rst_n(sys_rst_n)      ,
+      .dbg_mode(dbg_mode)    ,
   // counter control to register 
       .timer_en(timer_en_tmp),
-		.div_en(div_en_tmp)    ,
-		.div_val(div_val_tmp)  ,
-		.halt_req(halt_req_tmp),
-		.halt_ack(halt_ack_tmp),
+      .div_en(div_en_tmp)    ,
+      .div_val(div_val_tmp)  ,
+      .halt_req(halt_req_tmp),
+      .halt_ack(halt_ack_tmp),
   // counter control to counter
       .cnt_en(cnt_en_tmp)
   ); 
@@ -101,23 +111,18 @@ module timer_top
 		.pstrb(tim_pstrb)            , 
 		.wdata(tim_pwdata)           ,
   // counter to counter control 
-      .cnt_en(cnt_en_tmp)          ,
+    .cnt_en(cnt_en_tmp)          ,
   // counter to register 
-      .tdr0_wr_sel(tdr0_wr_sel_tmp),
+    .tdr0_wr_sel(tdr0_wr_sel_tmp),
 		.tdr1_wr_sel(tdr0_wr_sel_tmp),
 		.cnt(cnt_tmp)                ,
 		.cnt_clr(cnt_clr_tmp)
   );
-  
+
 endmodule 
   
   
   
-  
-  
-  
-  
-  s
   
   
   
